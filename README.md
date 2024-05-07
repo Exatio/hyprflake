2e996d86ca0bff8811b94b5b428e32f2750c9fc8
Exatio <exatio@proton.me>
2024-05-07 15:49:51 +0200

    Fixed screenshot script + updated README.MD + added some screenshots to assets dir

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# HyprFlake - My NixOS Hyprland flake
+# HyprFlake - My NixOS Hyprland config with Pywal !
+This repo contains my dotfiles for my Hyprland with Pywal setup on my NixOS system.
+
+## Setup
+
+- OS: [NixOS](https://nixos.org)
+- Window manager: [Hyprland](https://github.com/hyprwm/Hyprland)
+- Status bar: [Waybar](https://github.com/Alexays/Waybar)
+- Terminals: [Kitty](https://github.com/kovidgoyal/kitty) & [Alacritty](https://github.com/alacritty/alacritty)
+- Shell: [Zsh]( https://www.zsh.org/)
+- Current theme: [Pywal](https://github.com/dylanaraps/pywal)
+
+## Gallery
+
+|             Desktop              |
+| :------------------------------: |
+| ![desktop](./assets/screenshots/desktop1.png) |
+| ![desktop](./assets/screenshots/desktop2.png) |
+
+
+## Installation
+
+This flake is not ready-to-go, and is not meant to be for the moment.
+This section will be updated in the future.
+You need some prior NixOS knowledge to use this setup.
+A lot of options have to be modified according to your system, so you have to go through the setup to understand how it works and what to change.
+
+## Acknowledgements
+
+### Nix
+- [Dileep Kishore's nix config](https://github.com/dileep-kishore/nixos-hyprland) : Structure, reference
+
+### Hyprland
+- [Ja Koolit's setup](https://github.com/JaKooLit/Hyprland-Dots): Structure, reference, scripts
+- [Stephan Raabe's setup](https://gitlab.com/stephan-raabe/dotfiles): Reference, scripts
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,8 +2,8 @@
 
 iDIR="$HOME/hyprflake/assets/apps/dunst/icons"
 time=$(date +%Y-%m-%d-%H-%M-%S)
-dir="$(xdg-user-dir)/Images/screenshots"
-file="Capture_${time}_${RANDOM}.png"
+dir="$(xdg-user-dir)/Pictures/screenshots"
+file="Shot_${time}_${RANDOM}.png"
 
 notify_cmd_shot="notify-send -h string:x-canonical-private-synchronous:shot-notify -u low -i ${iDIR}/picture.png"
 
@@ -26,8 +26,10 @@ if [[ ! $(pidof tofi) ]]; then
 	option1="Area"
 	option2="Fullscreen"
 	option3="Window"
+	option4="eDP-1"
+	option5="DP-1"
 
-	options="$option1\n$option2\n$option3"
+	options="$option1\n$option2\n$option3\n$option4\n$option5"
 
 	choice=$(echo -e "$options" | tofi -c ~/.config/tofi/config-screenshot)
 	
@@ -41,15 +43,25 @@ if [[ ! $(pidof tofi) ]]; then
 		grim -g "$(slurp)" - | swappy -f -
 	    ;;
 	    $option2)
-		cd ${dir} && grim - | tee "$file" | wl-copy
 		sleep 2
+		cd ${dir} && grim - | tee "$file" | wl-copy
 		notify_view
 	    ;;
 	    $option3)
-		sleep 1
+		sleep 2
 		w_pos=$(hyprctl activewindow | grep 'at:' | cut -d':' -f2 | tr -d ' ' | tail -n1)
 		w_size=$(hyprctl activewindow | grep 'size:' | cut -d':' -f2 | tr -d ' ' | tail -n1 | sed s/,/x/g)
 		cd ${dir} && grim -g "$w_pos $w_size" - | tee "$file" | wl-copy
+		notify_view
+	    ;;
+	    $option4)
+		sleep 2
+		cd ${dir} && grim -o eDP-1 - | tee "$file" | wl-copy
+		notify_view
+	    ;;
+	    $option5)
+		sleep 2
+		cd ${dir} && grim -o DP-1 - | tee "$file" | wl-copy
 		notify_view
 	    ;;
 	esac
