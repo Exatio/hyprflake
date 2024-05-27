02fa228f22a1efdf78c250dcda225fa3a963098b
Exatio <exatio@proton.me>
2024-05-27 22:07:12 +0200

    add mullvad vpn for laptop

--- hosts/nixos-laptop/default.nix
+++ hosts/nixos-laptop/default.nix
@@ -16,6 +16,21 @@
 
   ];
 
+  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
+  };
+
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  };
+
   # For Thunar
   services.gvfs = {
     enable = true;
