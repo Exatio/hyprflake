193ea188ae62c6c3b025ee6116dbcb4c48613546
Exatio <exatio@proton.me>
2025-07-13 02:33:43 +0200

    pywalfox update

--- home/programs/pywal/default.nix
+++ home/programs/pywal/default.nix
@@ -16,6 +16,6 @@ in
 
   home.file.".mozilla/native-messaging-hosts/pywalfox.json".text = lib.replaceStrings [ "<path>" ] [
     "${pywalfox-wrapper}/bin/pywalfox-wrapper"
-  ] (lib.readFile "${pkgs.pywalfox-native}/lib/python3.12/site-packages/pywalfox/assets/manifest.json");
+  ] (lib.readFile "${pkgs.pywalfox-native}/lib/python3.13/site-packages/pywalfox/assets/manifest.json");
 
 }
