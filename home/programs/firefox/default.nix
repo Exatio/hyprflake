08ddadf0d2f34eac5a01751fa9d6ef962a0611ea
Exatio <exatio@proton.me>
2025-09-30 04:35:37 +0200

    blurredfox

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -8,12 +8,18 @@ let
   }; 
 in 
 {
-  # TODO replace : must download blurredfox repo to ~/.mozilla/firefox/default/chrome/blurredfox
   # you have to install the custom darkreader extension provided in the darkreader folder. 
   home.file = {
     ".mozilla/native-messaging-hosts/darkreader.json".text = builtins.toJSON
       (darkreaderManifest // { allowed_extensions = [ "darkreader@alexhulbert.com" ]; });
+    ".mozilla/firefox/default/chrome/layout-oneline.css".source = ./oneline.css;
+    ".mozilla/firefox/default/chrome/layout-twoline.css".source = ./twoline.css;
+    ".mozilla/firefox/default/chrome/blurredfox".source = builtins.fetchGit {
+      url = "https://github.com/eromatiya/blurredfox";
+      rev = "6976b5460f47bd28b4dc53bd093012780e6bfed3";
+    };
   };
+
 
   programs.firefox = {
     enable = true;
@@ -23,26 +29,29 @@ in
       name = "default";
       isDefault = true;
       settings = {
+        # both mentionned these
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        
+        # blurredfox mentionned these
+        "layers.acceleration.force-enabled" = true;
+        "gfx.webrender.all" = true;
+        "gfx.webrender.enabled" = true;
+        "layout.css.backdrop-filter.enabled" = true;
+        "svg.context-properties.content.enabled" = true;
+
+        # seaglass mentionned these
         "dom.w3c_touch_events.enabled" = 1;
         "widget.use-xdg-desktop-portal" = true;
         "apz.gtk.kinetic_scroll.enabled" = false;
+
       };
       userChrome = ''
         @import url('blurredfox/userChrome.css');
         @import url('userContent.css');
         @import url('layout-twoline.css');
       ''; 
-        #@import url('layout-oneline.css');
+        # @import url('layout-oneline.css');  # the seaglass creator uses this as well
     };
-  };
-  
-  home.file = {
-    ".mozilla/firefox/default/chrome/layout-oneline.css".source = ./oneline.css;
-  };
-
-  home.file = {
-    ".mozilla/firefox/default/chrome/layout-twoline.css".source = ./twoline.css;
   };
     
 }
