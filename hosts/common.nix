d97b8614bad067d719b9b67610adc2a54b3d4217
Exatio <exatio@proton.me>
2025-11-29 00:37:38 +0100

    [flake inputs] update

--- hosts/common.nix
+++ hosts/common.nix
@@ -9,6 +9,10 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
+      # Use Hyprland's Cachix to reduce build times
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
     gc = {
       automatic = true;
