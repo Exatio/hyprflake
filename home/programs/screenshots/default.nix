9956b12e92753aa0747b660fd01e9e90c418b323
Exatio <exatio@proton.me>
2025-11-29 02:31:44 +0100

    [screenshots] fix script

--- home/programs/screenshots/default.nix
+++ home/programs/screenshots/default.nix
@@ -10,7 +10,7 @@
   home.file.".config/swappy/config".text = ''
     [Default]
     save_dir=$HOME/Pictures/screenshots
-    save_filename_format=swappy-%Y%m%d-%H%M%S.png
+    save_filename_format=Shot_%Y-%m-%d-%H-%M-%S_Area.png
     show_panel=false
     line_size=5
     text_size=20
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,20 +2,9 @@
 
 iDIR="$HOME/hyprflake/assets/apps/dunst/icons"
 time=$(date +%Y-%m-%d-%H-%M-%S)
-dir="$(xdg-user-dir)/Pictures/screenshots"
-file="Shot_${time}_${RANDOM}.png"
+dir="$HOME/Pictures/screenshots"
 
 notify_cmd_shot="notify-send -h string:x-canonical-private-synchronous:shot-notify -u low -i ${iDIR}/picture.png"
-
-notify_view() {
-	${notify_cmd_shot} "Copied to clipboard."
-	if [[ -e "$dir/$file" ]]; then
-		${notify_cmd_shot} "Screenshot Saved."
-	else
-		${notify_cmd_shot} "Screenshot Deleted."
-	fi
-}
-
 
 if [[ ! $(pidof tofi) ]]; then
 
@@ -26,10 +15,8 @@ if [[ ! $(pidof tofi) ]]; then
 	option1="Area"
 	option2="Fullscreen"
 	option3="Window"
-	option4="eDP-1"
-	option5="DP-1"
 
-	options="$option1\n$option2\n$option3\n$option4\n$option5"
+	options="$option1\n$option2\n$option3"
 
 	choice=$(echo -e "$options" | tofi -c ~/.config/tofi/config-screenshot)
 	
@@ -43,28 +30,22 @@ if [[ ! $(pidof tofi) ]]; then
 		grim -g "$(slurp)" - | swappy -f -
 	    ;;
 	    $option2)
+		file="Shot_${time}_FS.png"
 		sleep 2
 		cd ${dir} && grim - | tee "$file" | wl-copy
-		notify_view
 	    ;;
 	    $option3)
+		file="Shot_${time}_Win.png"
 		sleep 2
 		w_pos=$(hyprctl activewindow | grep 'at:' | cut -d':' -f2 | tr -d ' ' | tail -n1)
 		w_size=$(hyprctl activewindow | grep 'size:' | cut -d':' -f2 | tr -d ' ' | tail -n1 | sed s/,/x/g)
 		cd ${dir} && grim -g "$w_pos $w_size" - | tee "$file" | wl-copy
-		notify_view
-	    ;;
-	    $option4)
-		sleep 2
-		cd ${dir} && grim -o eDP-1 - | tee "$file" | wl-copy
-		notify_view
-	    ;;
-	    $option5)
-		sleep 2
-		cd ${dir} && grim -o DP-1 - | tee "$file" | wl-copy
-		notify_view
 	    ;;
 	esac
+
+	if [[ -e "$dir/$file" && "$choice" != "$option1" ]]; then
+		${notify_cmd_shot} "Screenshot Saved."
+	fi
 
 else
 	pkill tofi
